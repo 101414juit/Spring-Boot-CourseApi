8c56fec506a6134295f60346db979970b1cf4857
karinkato <karinkato@192.168.1.101>
2018-07-28 13:50:06 +0530

    Course Api using CRUD operations

---
+++ Course-Api-Data/src/main/java/com/learnings/aman/springbootquick/topic/TopicService.java
@@ -0,0 +1,41 @@
+package com.learnings.aman.springbootquick.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	public List<Topic> getAllTopics(){
+		List <Topic> topics = new ArrayList<>();
+		topicRepository.findAll()
+		.forEach(topics::add);
+		return topics;
+	}
+	
+	public Optional<Topic> getTopic(String id) {
+		//return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id);
+	}
+
+	public void add(Topic topic) {
+		//topics.add(topic);
+		topicRepository.save(topic);
+	}
+
+	public void update(String id, Topic topic) {
+		topicRepository.save(topic);
+	}
+
+	public void delete(String id) {
+		topicRepository.deleteById(id);
+	}
+}
